857efb013423c599680d2d54200c65a893bbf395
omarfaiaz <faiazwork@gmail.com>
2024-05-20 04:28:46 +0600

    Second commit

--- assets/main.js
+++ assets/main.js
@@ -60,29 +60,172 @@ $(document).ready(function () {
     dots: false,
   });
 
+  // Collection Isotope Filter
+  var $grid = $("#collection-filter_block").isotope({
+    filter: "#shoes",
+  });
+
+  // filter items on button click
+  $(".collection-filter_btn").on("click", function () {
+    var filterValue = $(this).attr("data-filter");
+    console.log(filterValue);
+
+    $grid.isotope({ filter: filterValue });
+  });
+
+  // Best Seller Filter
+  $("#home-sort-by").on("change", async function () {
+    fetch("/collections/all?sort_by=" + $(this).val())
+      .then((response) => response.text())
+      .then((data) => {
+        let html_div = document.createElement("div");
+        html_div.innerHTML = data;
+
+        let html_dom = document.querySelector("#best-seller_grid").innerHTML;
+        console.log(html_div);
+
+        // document.querySelector("#best-seller_grid").innerHTML = html_div;
+        // update url without refreshing the page
+        // history.replaceState(null, null, '?sort_by=' + $(this).val());
+      })
+      .catch((error) => console.error("Error", error));
+  });
 
   // AJAX Cart
 
   const addToCartForms = document.querySelectorAll('form[action="/cart/add"]');
 
-  addToCartForms.forEach(cartForm => {
+  addToCartForms.forEach((cartForm) => {
     cartForm.addEventListener("submit", async function (e) {
       // Prevent Submission
       e.preventDefault();
 
-      await fetch('/cart/add', {
-        method: 'post',
-        body: new FormData(cartForm)
+      await fetch("/cart/add", {
+        method: "post",
+        body: new FormData(cartForm),
       });
 
       // Get New Object
-      const res = await fetch('/cart.json');
+      const res = await fetch("/cart.json");
       const cart = await res.json();
       console.log(cart);
-      
+    });
+  });
 
+  // Cart Drawer
+  function openCartDrawer() {
+    document.querySelector(".cart-drawer").classList.add("cart-drawer--active");
+  }
 
-    })
-  })
+  function closeCartDrawer() {
+    document
+      .querySelector(".cart-drawer")
+      .classList.remove("cart-drawer--active");
+  }
 
+  function updateCartItemCounts(count) {
+    document.querySelectorAll(".cart-count").forEach((el) => {
+      el.textContent = count;
+    });
+  }
+
+  async function updateCartDrawer() {
+    const res = await fetch("/?section_id=template-cart-drawer");
+    const text = await res.text();
+    const html = document.createElement("div");
+    html.innerHTML = text;
+
+    const newBox = html.querySelector(".cart-drawer").innerHTML;
+
+    document.querySelector(".cart-drawer").innerHTML = newBox;
+
+    addCartDrawerListeners();
+  }
+
+  function addCartDrawerListeners() {
+    // Update quantities
+    document
+      .querySelectorAll(".cart-drawer-quantity-selector button")
+      .forEach((button) => {
+        button.addEventListener("click", async () => {
+          // Get line item key
+          const rootItem =
+            button.parentElement.parentElement.parentElement.parentElement
+              .parentElement;
+          const key = rootItem.getAttribute("data-line-item-key");
+
+          // Get new quantity
+          const currentQuantity = Number(
+            button.parentElement.querySelector("input").value
+          );
+          const isUp = button.classList.contains(
+            "cart-drawer-quantity-selector-plus"
+          );
+          const newQuantity = isUp ? currentQuantity + 1 : currentQuantity - 1;
+
+          // Ajax update\
+          const res = await fetch("/cart/update.js", {
+            method: "post",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ updates: { [key]: newQuantity } }),
+          });
+          const cart = await res.json();
+
+          updateCartItemCounts(cart.item_count);
+
+          // Update cart
+          updateCartDrawer();
+        });
+      });
+
+    document
+      .querySelector(".cart-drawer-box")
+      .addEventListener("click", (e) => {
+        e.stopPropagation();
+      });
+
+    document
+      .querySelectorAll(".cart-drawer-header-right-close, .cart-drawer")
+      .forEach((el) => {
+        el.addEventListener("click", () => {
+          console.log("closing drawer");
+          closeCartDrawer();
+        });
+      });
+  }
+
+  addCartDrawerListeners();
+
+  document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      // Submit form with ajax
+      await fetch("/cart/add", {
+        method: "post",
+        body: new FormData(form),
+      });
+
+      // Get cart count
+      const res = await fetch("/cart.js");
+      const cart = await res.json();
+      updateCartItemCounts(cart.item_count);
+
+      // Update cart
+      await updateCartDrawer();
+
+      // Open cart drawer
+      openCartDrawer();
+    });
+  });
+
+  document.querySelectorAll('a[href="/cart"]').forEach((a) => {
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      openCartDrawer();
+    });
+  });
 });
